7d45760deb91d5489bf7de4e10e92625468789a1
mortallaniang22 <100224774+mortallaniang22@users.noreply.github.com>
2022-08-15 16:03:32 +0200

    Delete Nombre total d’appartements vendus au 1er semestre 2020.sql

--- Nombre total d’appartements vendus au 1er semestre 2020.sql
+++
@@ -1,5 +0,0 @@
-SELECT count(Id_Transac) as "Nombre total d'appartement vendu au premier semestre 2020"
-FROM transaction 
-JOIN biensimmobilier on (biensimmobilier.Id_Loc = transaction.Id_Loc)
-WHERE TypLoc = 'Appartement'
-AND Date_Mutation between '2020-01-01' and '2020-06-30';
