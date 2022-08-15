0156b622ef98559aac6186533066a21e9f06c0a4
mortallaniang22 <100224774+mortallaniang22@users.noreply.github.com>
2022-08-15 16:04:53 +0200

    Delete Prix moyen du mètre carré d’une maison en Île-de-France.sql

--- Prix moyen du mètre carré d’une maison en Île-de-France.sql
+++
@@ -1,9 +0,0 @@
-select round(avg(Val_Fonc/Surfcarrez),2) as "Prix moyen du mètre carré d’une maison en Île-de-France"
-from transaction
-inner join biensimmobilier on (transaction.Id_Loc = biensimmobilier.Id_Loc)
-inner join commune on ( biensimmobilier.Id_com = commune.Id_com)
-where TypLoc = 'maison'
-and CodeDepartement in (75, 77, 78, 91, 92, 93, 94, 95)
-
- 
-
